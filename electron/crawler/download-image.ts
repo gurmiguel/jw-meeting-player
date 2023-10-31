160724b6278158f326ec7b08f76aadd6d217699b
Gustavo Raise Miguel <gmiguel.saralux@pli.edu>
2023-10-31 01:58:03 -0300

    chore: cleanup old crwaling method files

--- electron/crawler/download-image.ts
+++
@@ -1,5 +0,0 @@
-import downloader from './Downloader'
-
-export async function downloadImage(url: string) {
-  return await downloader.enqueue(url, 'jpeg')
-}
--- electron/crawler/download-video.ts
+++
@@ -1,5 +0,0 @@
-import downloader from './Downloader'
-
-export async function downloadVideo(url: string) {
-  return await downloader.enqueue(url, 'mp4')
-}
